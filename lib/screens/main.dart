7b5438cf8c330775cb3b10f26bfc2de4d75a2329
Karlo Lugo <karlo.lugo@usjr.edu.ph>
2022-03-04 23:13:31 +0800

    Week 2

---
+++ lib/screens/main.dart
@@ -0,0 +1,22 @@
+import 'package:elec2b_review/screens/start_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Review App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
